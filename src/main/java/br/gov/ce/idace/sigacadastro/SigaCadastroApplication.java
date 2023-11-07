17601a172632b0d460ad1b729dfd5e43f196814f
Marcos Albano <marcos.rsilva@idace.ce.gov.br>
2023-11-07 16:29:15 -0300

    Configurações iniciais

---
+++ src/main/java/br/gov/ce/idace/sigacadastro/SigaCadastroApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ce.idace.sigacadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigaCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ce/idace/sigacadastro/SigaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ce.idace.sigacadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
